73c1feeadbe4f762573eac440721847d6d03963a
Nivaldo Cavalcanti <nivalduh@gmail.com>
2020-12-20 11:30:34 -0300

    Refactor: Putting variables inside state and setting app state change on answer check function

--- src/App.js
+++ src/App.js
@@ -2,14 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const value1 = Math.floor(Math.random() * 100);
-const value2 = Math.floor(Math.random() * 100);
-const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-const numQuestions = 0;
-const numCorrect = 0;
-
 class App extends Component {
+  state = {
+    value1: Math.floor(Math.random() * 100),
+    value2: Math.floor(Math.random() * 100),
+    value3: Math.floor(Math.random() * 100),
+    proposedAnswer: Math.floor(Math.random() * 3),
+    numQuestions: 0,
+    numCorrect: 0
+  }
+  checkAnswer = (answer) => {
+    const checksum = this.state.value1 + this.state.value2 + this.state.value3 === this.state.proposedAnswer + this.state.value1 + this.state.value2 + this.state.value3;
+    if(answer === checksum){
+      this.setState((currentState)=>({
+        value1: Math.floor(Math.random() * 100),
+        value2: Math.floor(Math.random() * 100),
+        value3: Math.floor(Math.random() * 100),
+        proposedAnswer: Math.floor(Math.random() * 3),
+        numQuestions: currentState.numQuestions+1,
+        numCorrect: currentState.numCorrect+1
+      }))
+    } else {
+      this.setState((currentState)=>({
+        value1: Math.floor(Math.random() * 100),
+        value2: Math.floor(Math.random() * 100),
+        value3: Math.floor(Math.random() * 100),
+        proposedAnswer: Math.floor(Math.random() * 3),
+        numQuestions: currentState.numQuestions+1
+      }))
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -20,12 +42,12 @@ class App extends Component {
         <div className="game">
           <h2>Mental Math</h2>
           <div className="equation">
-            <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
+            <p className="text">{`${this.state.value1} + ${this.state.value2} + ${this.state.value3} = ${this.state.proposedAnswer + this.state.value1 + this.state.value2 + this.state.value3}`}</p>
           </div>
-          <button>True</button>
-          <button>False</button>
+          <button onClick={() => this.checkAnswer(true)}>True</button>
+          <button onClick={() => this.checkAnswer(false)}>False</button>
           <p className="text">
-            Your Score: {numCorrect}/{numQuestions}
+            Your Score: {this.state.numCorrect}/{this.state.numQuestions}
           </p>
         </div>
       </div>
